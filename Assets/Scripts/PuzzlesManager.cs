f3864bc5758a5478895324127650d543d1fa6141
Pablo Villar <tracermc14@gmail.com>
2024-01-23 05:34:56 +0100

    Three puzzles adapted, mounted and finished, cinematics opening doors, ajdjustments of puzzle script

--- Assets/Scripts/PuzzlesManager.cs
+++ Assets/Scripts/PuzzlesManager.cs
@@ -6,55 +6,61 @@ using UnityEngine.Events;
 
 public class PuzzlesManager : MonoBehaviour
 {
-    public bool phase1Completed, phase2Completed, phase3Completed, phase4Completed, phase5Completed, phase6Completed;
-    public bool phase1Started, phase2Started, phase3Started, phase4Started, phase5Started, phase6Started;
-
+    private bool phase1Completed, phase2Completed, phase3Completed;
+    //public bool phase4Completed, phase5Completed, phase6Completed;
+    private bool phase1Started, phase2Started, phase3Started;
+    //public bool phase4Started, phase5Started, phase6Started;
+    
     public enum Phases { 
         Phase1, //Colocar las bolas de billar
         Phase2, //Patrón o combinación con cartas
         Phase3, //Ver algo con las gafas de vista
-        Phase4, //Mover manecillas del reloj
-        Phase5, //Romper electricidad con alcohol
-        Phase6 //Colocar bombilla en un lugar oscuro para ver algo escondido
+        //Phase4, //Mover manecillas del reloj
+        //Phase5, //Romper electricidad con alcohol
+        //Phase6 //Colocar bombilla en un lugar oscuro para ver algo escondido
     };
 
     private Phases currentPhase;
 
     //Phase1
-    public GameObject ball1, ball2, ball3, ball4, ball5, ball6, ball7, ball8, ball9, ball10, ball11, ball12, ball13, ball14, ball15, ball16;
+    //public GameObject ball1, ball2, ball3, ball4, ball5, ball6, ball7, ball8, ball9, ball10, ball11, ball12, ball13, ball14, ball15;
     //public Transform ball1CorrectPosition, ball2CorrectPosition, ball3CorrectPosition, ball4CorrectPosition, ball5CorrectPosition, ball6CorrectPosition;
     //public Transform ball7CorrectPosition, ball8CorrectPosition, ball9CorrectPosition, ball10CorrectPosition, ball11CorrectPosition, ball12CorrectPosition;
     //public Transform ball13CorrectPosition, ball14CorrectPosition, ball15CorrectPosition, ball16CorrectPosition;
-    public bool ball1InCorrectPosition, ball2InCorrectPosition, ball3InCorrectPosition, ball4InCorrectPosition, ball5InCorrectPosition, ball6InCorrectPosition;
-    public bool ball7InCorrectPosition, ball8InCorrectPosition, ball9InCorrectPosition, ball10InCorrectPosition, ball11InCorrectPosition, ball12InCorrectPosition;
-    public bool ball13InCorrectPosition, ball14InCorrectPosition, ball15InCorrectPosition, ball16InCorrectPosition;
+    private bool ball1InCorrectPosition, ball2InCorrectPosition, ball3InCorrectPosition, ball4InCorrectPosition, ball5InCorrectPosition, ball6InCorrectPosition;
+    private bool ball7InCorrectPosition, ball8InCorrectPosition, ball9InCorrectPosition, ball10InCorrectPosition, ball11InCorrectPosition, ball12InCorrectPosition;
+    private bool ball13InCorrectPosition, ball14InCorrectPosition, ball15InCorrectPosition;
 
     public UnityEvent OnPhase1Completed;
 
     //Phase2
-    public GameObject card1, card2, card3, card4, card5;
-    public bool card1InCorrectPosition, card2InCorrectPosition, card3InCorrectPosition, card4InCorrectPosition, card5InCorrectPosition;
+    //public GameObject card1, card2, card3, card4, card5, card6;
+    private bool card1InCorrectPosition, card2InCorrectPosition, card3InCorrectPosition, card4InCorrectPosition, card5InCorrectPosition, card6InCorrectPosition;
 
     public UnityEvent OnPhase2Completed;
 
     //Phase3
-    public GameObject hiddenObject;
+    
     public UnityEvent OnPhase3Completed;
-    public UnityEvent OnPhase4Completed;
+    //public UnityEvent OnPhase4Completed;
+
+    [Header("Hidden Object revealed by glasses")]
+    [Space]
+    public GameObject hiddenObject;
 
     //Phase4
-    public GameObject clock;
-    public GameObject clockHand1;
-    public GameObject clockHand2;
+    //public GameObject clock;
+    //public GameObject clockHand1;
+    //public GameObject clockHand2;
 
     //Phase5
-    public GameObject lightObject;
-    private Light lightComponent;
-    public GameObject shadowHiddenObject;
+    //public GameObject lightObject;
+    //private Light lightComponent;
+    //public GameObject shadowHiddenObject;
 
     //Phase6
-    public GameObject lightHiddenObject;
-    public bool lightInCorrectPosition;
+    //public GameObject lightHiddenObject;
+    //public bool lightInCorrectPosition;
 
     // Start is called before the first frame update
     void Start()
@@ -62,9 +68,9 @@ public class PuzzlesManager : MonoBehaviour
         currentPhase = Phases.Phase1;
         phase1Started = true;
         hiddenObject.SetActive(false);
-        lightComponent = lightObject.GetComponent<Light>();
-        shadowHiddenObject.SetActive(false);
-        lightHiddenObject.SetActive(false);
+        //lightComponent = lightObject.GetComponent<Light>();
+        //shadowHiddenObject.SetActive(false);
+        //lightHiddenObject.SetActive(false);
     }
 
     // Update is called once per frame
@@ -75,7 +81,7 @@ public class PuzzlesManager : MonoBehaviour
             case Phases.Phase1: //Comprobar boleanos de si las bolas están bien colocadas
                 if (ball1InCorrectPosition && ball2InCorrectPosition && ball3InCorrectPosition && ball4InCorrectPosition && ball5InCorrectPosition && ball6InCorrectPosition && 
                     ball7InCorrectPosition && ball8InCorrectPosition && ball9InCorrectPosition && ball10InCorrectPosition && ball11InCorrectPosition && ball12InCorrectPosition && 
-                    ball13InCorrectPosition && ball14InCorrectPosition && ball15InCorrectPosition && ball16InCorrectPosition)
+                    ball13InCorrectPosition && ball14InCorrectPosition && ball15InCorrectPosition)
                 {
                     phase1Completed = true;
                     currentPhase = Phases.Phase2;
@@ -86,10 +92,11 @@ public class PuzzlesManager : MonoBehaviour
                 phase1Started = false;
                 phase2Started = true;
 
-                if (card1InCorrectPosition && card2InCorrectPosition && card3InCorrectPosition && card4InCorrectPosition && card5InCorrectPosition)
+                if (card1InCorrectPosition && card2InCorrectPosition && card3InCorrectPosition && card4InCorrectPosition && card5InCorrectPosition && card6InCorrectPosition)
                 {
                     phase2Completed = true;
                     currentPhase = Phases.Phase3;
+                    OnPhase2Completed.Invoke();
                 }
                 break;
             case Phases.Phase3: //Si se ha cogido las gafas glasses on y hiddenObject aparece
@@ -102,7 +109,7 @@ public class PuzzlesManager : MonoBehaviour
                     //Ver que se hace después hasta pasar a siguiente fase
                 }
                 break;
-            case Phases.Phase4: //Ampliar reloj, con mandos cambiar rotación de las manecillas
+            /*case Phases.Phase4: //Ampliar reloj, con mandos cambiar rotación de las manecillas
                 phase3Started = false;
                 phase4Started = true;
                 break;
@@ -130,7 +137,7 @@ public class PuzzlesManager : MonoBehaviour
                     //Algo pasa, encender la luz?
                 }
 
-                break;
+                break;*/
             default:
                 break;
         }
@@ -288,16 +295,6 @@ public class PuzzlesManager : MonoBehaviour
         ball15InCorrectPosition = false;
     }
 
-    public void Ball16Correct()
-    {
-        ball16InCorrectPosition = true;
-    }
-
-    public void Ball16Incorrect()
-    {
-        ball16InCorrectPosition = false;
-    }
-
     //Métodos phase2
     public void Card1Correct()
     {
@@ -349,8 +346,18 @@ public class PuzzlesManager : MonoBehaviour
         card5InCorrectPosition = false;
     }
 
+    public void Card6Correct()
+    {
+        card6InCorrectPosition = true;
+    }
+
+    public void Card6Incorrect()
+    {
+        card6InCorrectPosition = false;
+    }
+
     //Métodos phase 6
-    public void LightCorrect()
+    /*public void LightCorrect()
     {
         lightInCorrectPosition = true;
     }
@@ -358,5 +365,5 @@ public class PuzzlesManager : MonoBehaviour
     public void LightIncorrect()
     {
         lightInCorrectPosition = false;
-    }
+    }*/
 }
